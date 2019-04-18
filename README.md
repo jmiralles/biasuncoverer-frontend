61198f630f1ae933913d6524cd29b353931ba32b
jmiralles <jmirallespaez@gmail.com>
2019-04-18 23:20:46 +0200

    docker and nginx

---
+++ README.md
@@ -0,0 +1,9 @@
+# biasUncovered
+
+## To run in dev
+
+docker-compose -f docker-compose.dev.yml up
+
+## To run in prod
+
+docker-compose up
---
+++ front/webpack.config.base.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+const path = require("path");
+
+//const postcssPresetEnv = require("postcss-preset-env");
+
+module.exports = {
+  entry: ["./src/main.js"],
+  output: {
+    publicPath: "/dist/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: "vue-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-syntax-dynamic-import"]
+          }
+        }
+      },
+      {
+        test: /\.styl(us)?$/,
+        exclude: /node_modules/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "stylus-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [require("autoprefixer")]
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.template.html"
+    }),
+    new VueLoaderPlugin()
+  ]
+};
